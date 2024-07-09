db779b51c4feeca4c0ffa86edc78adbded3077e2
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2024-07-09 23:39:41 +0300

    Update exercises.js

--- 01_Day_Introduction/exercises.js
+++ 01_Day_Introduction/exercises.js
@@ -7,7 +7,7 @@ easy to reuse and informative*/
 //DATA TYPES EXERCISES
 let nameAndSurname = "Arzu Demir"; //string
 let age = 15; //number
-let isStudent = true //boolean
+let isStudent = true; //boolean
 let schoolName = undefined; //undefined
 let secondSchool = null; //object type
 console.log(nameAndSurname, age, isStudent, schoolName, secondSchool)
@@ -29,4 +29,3 @@ const myBirthCountry = "Turkey";
 const myBirthCity = "Sanliurfa";
 const myBirthYear = 2008;
 console.log(myBirthCountry, myBirthCity, myBirthYear)
-
