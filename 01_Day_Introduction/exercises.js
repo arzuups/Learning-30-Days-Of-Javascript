c923e6b1689ec3e9ea584515d166a5c9fb591e0d
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2024-07-15 00:13:01 +0300

    Update exercises.js

--- 01_Day_Introduction/exercises.js
+++ 01_Day_Introduction/exercises.js
@@ -4,7 +4,7 @@
 /*comments can make code readable,
 easy to reuse and informative*/
 
-//DATA TYPES EXERCISES
+//1-)DATA TYPES EXERCISES
 let nameAndSurname = "Arzu Demir"; //string
 let age = 15; //number
 let isStudent = true; //boolean
@@ -12,20 +12,20 @@ let schoolName = undefined; //undefined
 let secondSchool = null; //object type
 console.log(nameAndSurname, age, isStudent, schoolName, secondSchool)
 
-//VARIABLES
+//2-)VARIABLES
 //let
 let teacherName = "Lenora";
 let herAge = 40;
 let isSheTeacher = true;
-console.log(teacherName, herAge , isSheTeacher)
+console.log(teacherName, herAge , isSheTeacher);
 //let 2 exercise
 let hisName = "Jack",
 hisAge = 30,
 heIsStudent = true;
-console.log(hisName, hisAge, heIsStudent)
+console.log(hisName, hisAge, heIsStudent);
 
 //const
 const myBirthCountry = "Turkey";
 const myBirthCity = "Sanliurfa";
 const myBirthYear = 2008;
-console.log(myBirthCountry, myBirthCity, myBirthYear)
+console.log(myBirthCountry, myBirthCity, myBirthYear);
