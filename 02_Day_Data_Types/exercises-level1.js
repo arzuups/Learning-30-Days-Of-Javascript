5b327126e36b6c7b819d0afca4143bb232a0ce1a
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2024-07-15 00:15:38 +0300

    Update exercises-level1.js

--- 02_Day_Data_Types/exercises-level1.js
+++ 02_Day_Data_Types/exercises-level1.js
@@ -42,56 +42,56 @@ console.log(string7.split(", "));
 
 //9-)
 let string8 = "30 Days of Javascript";
-console.log(string8.replace("Javascript", "Python")) //30 Days of Python
+console.log(string8.replace("Javascript", "Python")); //30 Days of Python
 
 //10-)
 let string9 = "30 Days of Javascript";
-console.log(string9.charAt(15)) //s
+console.log(string9.charAt(15)); //s
 
 //11-)
 let string10 = "30 Days of Javascript";
-console.log(string10.charCodeAt('J')) //51 //ASCII code
+console.log(string10.charCodeAt('J')); //51 //ASCII code
 
 //12-)
 let string11 = "30 Days of Javascript";
-console.log(string11.indexOf("of")) //8
+console.log(string11.indexOf("of")); //8
 
 //13-)
 let string12 = "30 Days of Javascript";
-console.log(string12.lastIndexOf("of")) //8
+console.log(string12.lastIndexOf("of")); //8
 
 //14-)
 let string13 = 'You cannot end a sentence with because because because is a conjunction';
-console.log(string13.indexOf("because")) ///31
+console.log(string13.indexOf("because")); ///31
 
 //15-)
 let string14 = 'You cannot end a sentence with because because because is a conjunction';
-console.log(string14.lastIndexOf("because")) //47
+console.log(string14.lastIndexOf("because")); //47
 
 //16-)
 let string15 = 'You cannot end a sentence with because because because is a conjunction';
-console.log(string15.search("because")) //31
+console.log(string15.search("because")); //31
 
 //17-)
 let string16 = '  30 Days of Javascript  ';
-console.log(string16.trim()) //30 Days of Javascript
+console.log(string16.trim()); //30 Days of Javascript
 
 //18-)
 let string17 = "30 Days of Javascript";
-console.log(string17.startsWith('30 Days')) //true
+console.log(string17.startsWith('30 Days')); //true
 
 //19-)
 let string18 = "30 Days of Javascript";
-console.log(string18.endsWith('Javascript')) //true
+console.log(string18.endsWith('Javascript')); //true
 
 //20-)
 let string19 = "30 Days of Javascript";
-console.log(string19.match(/a/gi)) //[ 'a', 'a', 'a' ]
+console.log(string19.match(/a/gi)); //[ 'a', 'a', 'a' ]
 
 //21-)
 let string20 = "30 Days of ";
-console.log(string20.concat('Javascript')) //30 Days of Javascript
+console.log(string20.concat('Javascript')); //30 Days of Javascript
 
 //22-)
 let string21 = "30 Days of Javascript";
-console.log(string21.repeat(2)) //30 Days of Javascript30 Days of Javascript
+console.log(string21.repeat(2)); //30 Days of Javascript30 Days of Javascript
