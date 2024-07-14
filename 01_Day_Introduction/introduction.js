70c367bffd65defca6289e88b7f729e27f4aa8a9
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2024-07-15 00:14:22 +0300

    Update introduction.js

--- 01_Day_Introduction/introduction.js
+++ 01_Day_Introduction/introduction.js
@@ -2,11 +2,11 @@
 //console.log("Helloooww:)");
 
 //DATA TYPES
-console.log(typeof "Arzu DEMIR") //string data type
-console.log(typeof 5) //number data type
-console.log(typeof true) //boolean data type
-console.log(typeof undefined) //undefined data type
-console.log(typeof null) //object data type
+console.log(typeof "Arzu DEMIR"); //string data type
+console.log(typeof 5); //number data type
+console.log(typeof true); //boolean data type
+console.log(typeof undefined); //undefined data type
+console.log(typeof null); //object data type
 
 //VARIABLES
 // let = For a variable that changes at a different time, we use let.
@@ -16,10 +16,10 @@ let country = "Turkey";
 let city = "Sanliurfa";
 let age = 15;
 let isStudent = true;
-console.log(firstName, lastName, country, city, age, isStudent )
+console.log(firstName, lastName, country, city, age, isStudent );
 
 // const =  If the data does not change at all, we use const.
 const gravity = 9.81;
 const PI = 3.14;
 const boilingPoint = 100;
-console.log(gravity, PI, boilingPoint)
+console.log(gravity, PI, boilingPoint);
