cc3678d65144b78085950338d8ff78ba56fc74d6
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2024-07-15 00:17:22 +0300

    Update exercises-level3.js

--- 02_Day_Data_Types/exercises-level3.js
+++ 02_Day_Data_Types/exercises-level3.js
@@ -10,11 +10,11 @@ let string2 = 'You cannot end a sentence with because because because is a conju
 let word2 = 'because';
 let parts2 = string2.split(word2);
 let count2 = parts2.length - 1;
-console.log(`The '${word2}' occurs ${count2} in the sentence`) //The 'because' occurs 3 in the sentence
+console.log(`The '${word2}' occurs ${count2} in the sentence`); //The 'because' occurs 3 in the sentence
 
 //3-)
 const sentence3 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-console.log(sentence3.replace('%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching', 'I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher.This 30 Days Of Javascript is also the result of love of teaching. '))
+console.log(sentence3.replace('%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching', 'I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher.This 30 Days Of Javascript is also the result of love of teaching. '));
 /*output:
 I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher.This 30 Days Of Javascript is also the result of love of teaching.
 */
