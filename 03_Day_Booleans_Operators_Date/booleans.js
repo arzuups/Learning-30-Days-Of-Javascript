279275656e6a866220e458ce7cdaf1a717c1a3e7
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2024-07-15 00:20:04 +0300

    Update booleans.js

--- 03_Day_Booleans_Operators_Date/booleans.js
+++ 03_Day_Booleans_Operators_Date/booleans.js
@@ -1,1 +1,30 @@
+let isLightOn = true;
+let isHungry = false;
+let isMarried = false;
+let isStudent = true;
+let isRaining = false;
+console.log(isLightOn, isHungry, isMarried, isStudent, isRaining); //true false false true false
 
+let trueValue = 4 > 3; //true
+let falseValue = 5 < 2; //false
+console.log(trueValue, falseValue);
+
+//TRUTHY VALUES
+//all numbers(positive and negative) are truth except zero
+//all strings are truthy except an empty string[""]
+//the boolean true
+
+//FALSY VALUES
+//0
+//0n
+//null
+//undefined
+//NaN
+//the boolean false
+//'', "", ``, empty string
+
+let firstName
+console.log(firstName); //undefined
+
+let job = null;
+console.log(job); //null
